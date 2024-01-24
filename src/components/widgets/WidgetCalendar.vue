1325606964d8a31c4c2d6d6036f017bc3567ee54
c1chy1 <cichy08081987pp@gmail.com>
2024-01-25 00:15:35 +0100

    widget calendar #done

---
+++ src/components/widgets/WidgetCalendar.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <VaCard style="width: 100%;" class="pt-4">
+    <table class="table table-bordered mx-auto ">
+
+      <tr>
+        <td>
+          <VaDatePicker
+            v-model="date"
+          />
+        </td>
+      </tr>
+    </table>
+  </VaCard>
+</template>
+
+<script setup lang="ts">
+
+
+import { ref } from 'vue'
+import { VaDatePicker } from 'vuestic-ui'
+
+const date = ref()
+
+date.value = new Date()
+
+
+
+</script>
